df2d68a3e6592dbe4ff4869cdab86bd48677721c
Andre <andre.creten@swift.com>
2025-11-25 15:19:29 +0100

    Nice personal adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -10,7 +10,9 @@ def print_sleep(message, wait_time):
 def combat(weapon):
     print_sleep(f"The {enemy} attacks you!", 2)
     if weapon == "dagger":
-        print_sleep(f"You feel a bit under-prepared for this, what with only having a tiny {weapon}.", 2)
+        print_sleep(
+            f"You feel a bit under-prepared for this, what with only having a tiny {weapon}.",
+            2)
     choice = input("Would you like to (1) fight or (2) run away?")
     if choice == '1':
         if weapon == "dagger":
@@ -18,12 +20,19 @@ def combat(weapon):
             print_sleep(f"but your {weapon} is no match for the {enemy}.", 2)
             print_sleep(f"You have been defeated!""", 2)
         elif weapon == "sword":
-            print_sleep(f"As the {enemy} moves to attack, you unsheath your new sword.", 2)
-            print_sleep(f"The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.", 3)
-            print_sleep(f"But the {enemy} takes one look at your shiny new toy and runs away!", 3)
-            print_sleep(f"You have rid the town of the {enemy}. You are victorious!", 3)
+            print_sleep(
+                f"As the {enemy} moves to attack, you unsheath your new sword.", 2)
+            print_sleep(
+                f"The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.",
+                3)
+            print_sleep(
+                f"But the {enemy} takes one look at your shiny new toy and runs away!", 3)
+            print_sleep(
+                f"You have rid the town of the {enemy}. You are victorious!", 3)
     elif choice == '2':
-        print_sleep("You run back into the field. Luckily, you don't seem to have been followed.", 2)
+        print_sleep(
+            "You run back into the field. Luckily, you don't seem to have been followed.",
+            2)
         where_to()
 
 
@@ -41,11 +50,17 @@ def play_again():
 
 
 def intro():
-    print_sleep("You find yourself standing in an open field, filled with grass and yellow wildflowers.", 3)
-    print_sleep(f"Rumor has it that a {enemy} is somewhere around here, and has been terrifying the nearby village.", 3)
+    print_sleep(
+        "You find yourself standing in an open field, filled with grass and yellow wildflowers.",
+        3)
+    print_sleep(
+        f"Rumor has it that a {enemy} is somewhere around here, and has been terrifying the nearby village.",
+        3)
     print_sleep("In front of you is a house.", 2)
     print_sleep("To your right is a dark cave.", 2)
-    print_sleep(f"In your hand you hold your trusty (but not very effective) {weapon}.", 2)
+    print_sleep(
+        f"In your hand you hold your trusty (but not very effective) {weapon}.",
+        2)
 
 
 def where_to():
@@ -65,7 +80,9 @@ def where_to():
 
 def house():
     print_sleep("You approach the door of the house.", 2)
-    print_sleep(f"You are about to knock when the door opens and out steps a {enemy}.", 2)
+    print_sleep(
+        f"You are about to knock when the door opens and out steps a {enemy}.",
+        2)
     print_sleep(f"Eep! This is the {enemy}'s house!", 2)
     combat(weapon)
 
@@ -75,12 +92,15 @@ def cave():
     global weapon
     print_sleep("You peer cautiously into the cave.", 2)
     if cave_visited:
-        print_sleep("You've been here before, and gotten all the good stuff. It's just an empty cave now.", 2)
+        print_sleep(
+            "You've been here before, and gotten all the good stuff. It's just an empty cave now.",
+            2)
     elif cave_visited is False:
         print_sleep("It turns out to be only a very small cave.", 2)
         print_sleep("Your eye catches a glint of metal behind a rock.", 2)
         print_sleep("You have found the magical Sword of Ogoroth!", 2)
-        print_sleep(f"You discard your silly old {weapon} and take the sword with you.", 2)
+        print_sleep(
+            f"You discard your silly old {weapon} and take the sword with you.", 2)
         weapon = "sword"
     cave_visited = True
     print_sleep("You walk back out to the field.", 2)
@@ -99,4 +119,3 @@ while game_state == 'running':
     where_to()
 
     game_state = play_again()
-
